3a1d698444998e54ae4f8516d2acf958fe099ee6
AndreyErmolkin <78263246+AndreyErmolkin@users.noreply.github.com>
2021-03-18 23:23:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW_8
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Dispatch { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Student> students = new Queue<Student>();
+            students.Enqueue(new Student { Name = "Sam", Surname = "Robin", Dispatch = true });
+            students.Enqueue(new Student { Name = "Tom", Surname = "Koshkin", Dispatch = false });
+            students.Enqueue(new Student { Name = "Olga", Surname = "Kudrina", Dispatch = false });
+            students.Enqueue(new Student { Name = "Masha", Surname = "Trelina", Dispatch = true });
+
+            foreach (var std in students)
+            {
+                if (std.Dispatch == true)
+                {
+                    Console.WriteLine($"Student  {std.Name} {std.Surname}  sent homework and get coffee");
+                }
+                else
+                {
+                    Console.WriteLine($"Student {std.Name} {std.Surname}  didn’t submit homework and didn’t get coffee");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
